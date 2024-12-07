3115d363a1ea30e5f7a685af627c7eec0411cbbb
Миннуллин Ирек <irekminn4@gmail.com>
2024-12-07 15:28:05 +0300

    lesson23

---
+++ lesson 23/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+
+struct Dog;
+struct Owner
+{
+    Owner() {
+
+    }
+
+    ~Owner(){
+
+    }
+    
+    Dog* dog;
+
+};
+
+struct Dog
+{
+    Dog() {
+    
+    }
+    
+    ~Dog() {
+    }
+
+    void voice() {
+        std::cout << "bark " << std::endl;
+    }
+    Owner* owner;
+};
+
+int main() {
+    
+    Owner* owner = new Owner;
+    Dog* dog = new Dog;
+
+    owner->dog = dog;
+    dog->owner = owner;
+
+    return 0;
+}
